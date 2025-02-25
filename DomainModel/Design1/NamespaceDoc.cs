f1e08fe0191e82a1eeb78ea643c92d1dc95ee4dd
vijay@envivo <vijay.patel@envivo.co.uk>
2025-02-25 11:29:46 +0000

    improv: Added NamespaceDoc, to make it easier to hide namespaces during demos

---
+++ DomainModel/Design1/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿using Envivo.Fresnel.ModelAttributes;
+
+namespace LabCommsModel.Design1
+{
+    [Visible(true)]
+    internal class NamespaceDoc
+    {
+
+    }
+}
---
+++ DomainModel/Design2/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿using Envivo.Fresnel.ModelAttributes;
+
+namespace LabCommsModel.Design2
+{
+    [Visible(true)]
+    internal class NamespaceDoc
+    {
+
+    }
+}
